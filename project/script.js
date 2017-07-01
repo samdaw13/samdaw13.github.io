ba0b00d1cd00b440ef79a9c81dab86882dc1bc02
samdaw13 <samdaw13@gmail.com>
2017-07-01 12:05:18 -0600

    project

---
+++ project/script.js
@@ -0,0 +1,11 @@
+function getJSON(url) {
+    return fetch(url)
+        .then(function (response) {
+        return response.json();
+    })
+        .catch(function (error) {
+        console.log(error);
+    });
+}
+var url = "/info.json";
+getJSON(url);
