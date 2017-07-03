7e47e15f15185fc1d38c8f5ca06f59b088e97dd7
samdaw13 <samdaw13@gmail.com>
2017-07-02 22:47:54 -0600

    final touches

--- project/script.js
+++ project/script.js
@@ -1,26 +1,28 @@
+"use strict";
 function getJSON(url) {
     return fetch(url)
         .then(function (response) {
-        return response.json();
-    })
+            return response.json();
+        })
         .catch(function (error) {
-        console.log(error);
-    });
+            console.log(error);
+        });
 }
-function getPeople(url){
-    getJSON(url).then(function(data){
+
+function getPeople(url) {
+    getJSON(url).then(function (data) {
         var dictators = data.people;
 
         var firstRow = document.getElementById('row-one');
         var secondRow = document.getElementById('row-two');
-        dictators.forEach(function(person){
+        dictators.forEach(function (person) {
             var tableData = document.createElement('td');
             var image = document.createElement('img');
             image.setAttribute('src', person.image);
             tableData.appendChild(image);
             firstRow.appendChild(tableData);
         });
-        dictators.forEach(function(person){
+        dictators.forEach(function (person) {
             var tableData = document.createElement('td');
             var anchor = document.createElement('a');
             anchor.setAttribute('href', '#');
@@ -28,8 +30,8 @@ function getPeople(url){
             tableData.appendChild(anchor);
 
             secondRow.appendChild(tableData);
-            anchor.addEventListener('click', function(event){
-               event.preventDefault();
+            anchor.addEventListener('click', function (event) {
+                event.preventDefault();
                 document.getElementById('information').style.display = 'block';
 
                 document.getElementById('nickname').innerHTML = person.nickname;
@@ -47,47 +49,159 @@ function getPeople(url){
     });
 }
 
-function getEarlyVictories(url){
-    getJSON(url).then(function(data){
+function getEarlyVictories(url) {
+    getJSON(url).then(function (data) {
         var earlyBattles = data.earlyAxisVictories;
         var rowOne = document.getElementById('first-row');
         var rowTwo = document.getElementById('second-row');
         var rowThree = document.getElementById('third-row');
         var rowFour = document.getElementById('fourth-row');
-        for(var i = 0; i <= 2; i++){
+       for(var i = 0; i < 3; i++){
+           var image = document.createElement('img');
+           image.setAttribute('src', earlyBattles[i].image);
+           var tableRowOne = document.createElement('td');
+           tableRowOne.appendChild(image);
+           rowOne.appendChild(tableRowOne);
+       }
+        for(var i = 3; i < 6; i++){
+            var image2 = document.createElement('img');
+            image2.setAttribute('src', earlyBattles[i].image);
+            var tableRowThree = document.createElement('td');
+            tableRowThree.appendChild(image2);
+            rowThree.appendChild(tableRowThree);
+        }
+        for(let i = 0; i < 3; i++){
 
             var tableData = document.createElement('td');
-            var image = document.createElement('img');
-            image.setAttribute('src', earlyBattles[i].image);
-            rowOne.appendChild(tableData);
 
-            var tableData1 = document.createElement('td');
+            var anchor1 = document.createElement('a');
+            anchor1.setAttribute('href', "#");
 
+            anchor1.innerHTML = earlyBattles[i].battleName;
+            tableData.appendChild(anchor1);
+            rowTwo.appendChild(tableData);
 
-            var battleStuff = document.createTextNode(earlyBattles[i].battleName);
-            tableData.appendChild(image);
-            tableData.appendChild(battleStuff);
-           rowTwo.appendChild(tableData1);
+            anchor1.addEventListener('click', function (event) {
+
+                event.preventDefault();
+                document.getElementById('information').style.display = 'block';
+                document.getElementById('axis-casualties').innerHTML = earlyBattles[i].casualtiesAxis;
+
+                document.getElementById('allied-casualties').innerHTML = earlyBattles[i].casualtiesAllied
+
+                document.getElementById('date-began').innerHTML = earlyBattles[i].dateBegan
+
+                document.getElementById('date-ended').innerHTML = earlyBattles[i].dateEnded
+
+                document.getElementById('victor').innerHTML = earlyBattles[i].victor;
+
+                document.getElementById('image').innerHTML = earlyBattles[i].image2;
+
+                document.getElementById('battle-description').innerHTML = earlyBattles[i].description;
+            });
 
         }
-        for(var i = 3; i <= 6; i++){
+        for(let i = 3; i < 6; i++){
 
             var tableData = document.createElement('td');
-            var image = document.createElement('img');
-            image.setAttribute('src', earlyBattles[i].image);
-            rowThree.appendChild(tableData);
 
-            var tableData1 = document.createElement('td');
+            var anchor1 = document.createElement('a');
+            anchor1.setAttribute('href', "#");
 
+            anchor1.innerHTML = earlyBattles[i].battleName;
+            tableData.appendChild(anchor1);
+            rowFour.appendChild(tableData);
 
-            var battleStuff = document.createTextNode(earlyBattles[i].battleName);
-            tableData.appendChild(image);
-            tableData.appendChild(battleStuff);
-            rowFour.appendChild(tableData1);
+            anchor1.addEventListener('click', function (event) {
+
+                event.preventDefault();
+                document.getElementById('information').style.display = 'block';
+                document.getElementById('axis-casualties').innerHTML = earlyBattles[i].casualtiesAxis;
+
+                document.getElementById('allied-casualties').innerHTML = earlyBattles[i].casualtiesAllied
+
+                document.getElementById('date-began').innerHTML = earlyBattles[i].dateBegan
+
+                document.getElementById('date-ended').innerHTML = earlyBattles[i].dateEnded
+
+                document.getElementById('victor').innerHTML = earlyBattles[i].victor;
+
+                document.getElementById('image').innerHTML = earlyBattles[i].image2;
+
+                document.getElementById('battle-description').innerHTML = earlyBattles[i].description;
+            });
 
         }
+        /*for(var i = 0; i < 3; i++){
+            var tableData = document.createElement('td');
 
-    });
+            var anchor1 = document.createElement('a');
+            anchor1.setAttribute('href', "#");
+
+            anchor1.innerHTML = earlyBattles[i].battleName;
+            tableData.appendChild(anchor1);
+            rowTwo.appendChild(tableData);
+
+            anchor1.addEventListener('click', function (event) {
+
+                event.preventDefault();
+                document.getElementById('information').style.display = 'block';
+                document.getElementById('axis-casualties').innerHTML = earlyBattles[i].casualtiesAxis;
+
+                document.getElementById('allied-casualties').innerHTML = earlyBattles[i].casualtiesAllied
+
+                document.getElementById('date-began').innerHTML = earlyBattles[i].dateBegan
+
+                document.getElementById('date-ended').innerHTML = earlyBattles[i].dateEnded
+
+                document.getElementById('victor').innerHTML = earlyBattles[i].victor;
+
+                document.getElementById('image').innerHTML = earlyBattles[i].image2;
+
+                document.getElementById('battle-description').innerHTML = earlyBattles[i].description;
+            });
+
+        }*/
+    }); /*earlyBattles.forEach(function(battle){
+            var image = document.createElement('img');
+            image.setAttribute('src', battle.image);
+            var tableRowOne = createElement('td');
+            tableRowOne.appendChild(image);
+            rowOne.appendChild(tableRowOne);
+        });
+        earlyBattles.forEach(function (battle) {
+            var tableData = document.createElement('td');
+
+            var anchor1 = document.createElement('a');
+            anchor1.setAttribute('href', "#");
+
+            anchor1.innerHTML = battle.battleName;
+            tableData.appendChild(anchor1);
+            rowTwo.appendChild(tableData);
+
+            anchor1.addEventListener('click', function (event) {
+
+                event.preventDefault();
+                document.getElementById('information').style.display = 'block';
+                document.getElementById('axis-casualties').innerHTML = battle.casualtiesAxis;
+
+                document.getElementById('allied-casualties').innerHTML = battle.casualtiesAllied
+
+                document.getElementById('date-began').innerHTML = battle.dateBegan
+
+                document.getElementById('date-ended').innerHTML = battle.dateEnded
+
+                document.getElementById('victor').innerHTML = battle.victor;
+
+                document.getElementById('image').innerHTML = battle.image2;
+
+                document.getElementById('battle-description').innerHTML = battle.description;
+            });
+        });
+
+
+    });*/
+
 }
 var url = "//samdaw13.github.io/project/info.json";
 getPeople(url);
